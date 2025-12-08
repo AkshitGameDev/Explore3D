34fa9e52f1fe6f055aa49e89e9a9e56d5185e9b6
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2025-12-07 19:55:57 -0500

    Update AimStateManager.cs

--- Explorer3D/Assets/scripts/AimStateManager.cs
+++ Explorer3D/Assets/scripts/AimStateManager.cs
@@ -1,7 +1,10 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class AimStateManager : MonoBehaviour
 {
+
+    public Cinemachinem aimCam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
