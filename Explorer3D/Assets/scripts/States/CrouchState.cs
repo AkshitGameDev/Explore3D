279d9a22d4020ef76e6a719b0d1273978593a065
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2025-12-11 18:36:57 -0500

    basic animations added

--- Explorer3D/Assets/scripts/States/CrouchState.cs
+++ Explorer3D/Assets/scripts/States/CrouchState.cs
@@ -5,11 +5,23 @@ public class CrouchState : MovementBaseState
  
     public override void EnterState(MovementSateManager movement)
     {
-
+        movement.anim.SetBool("crouch", true);
     }
 
    public override void UpdateState(MovementSateManager movement)
     {
-      
+        if(Input.GetKeyDown(KeyCode.LeftShift)) ExitState(movement, movement.runState);
+
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if(movement.dir.magnitude > 0.1) ExitState(movement, movement.walkState);
+            else ExitState(movement, movement.idleState);
+        }
+
+    }
+    void ExitState(MovementSateManager movement, MovementBaseState state)
+    {
+        movement.anim.SetBool("crouch", false);
+        movement.SwitchState(state);
     }
 }
--- Explorer3D/Assets/scripts/States/RunState.cs
+++ Explorer3D/Assets/scripts/States/RunState.cs
@@ -5,11 +5,18 @@ public class RunState : MovementBaseState
 
     public override void EnterState(MovementSateManager movement)
     {
-
+        movement.anim.SetBool("run", true);
     }
 
     public override void UpdateState(MovementSateManager movement)
     {
+        if(Input.GetKeyUp(KeyCode.LeftShift)) ExitState(movement, movement.walkState);
+        else if(movement.dir.magnitude == 0) ExitState(movement, movement.idleState);
 
+    }
+    void ExitState(MovementSateManager movement, MovementBaseState state)
+    {
+        movement.anim.SetBool("run", false);
+        movement.SwitchState(state);
     }
 }
