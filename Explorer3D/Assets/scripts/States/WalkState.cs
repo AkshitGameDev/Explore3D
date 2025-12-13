947e4d8db50ddfcf32a0c93475b52ce3f47ea68d
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2025-12-13 16:59:21 -0500

    movement alter speeds added

--- Explorer3D/Assets/scripts/States/WalkState.cs
+++ Explorer3D/Assets/scripts/States/WalkState.cs
@@ -13,6 +13,11 @@ public class WalkState : MovementBaseState
         if(Input.GetKey(KeyCode.LeftShift)) ExitState(movement, movement.runState);
         else if (Input.GetKey(KeyCode.LeftControl)) ExitState(movement, movement.crouchState);
         else if (movement.dir.magnitude < 0.1f) ExitState(movement, movement.idleState);
+
+        if(movement.returnVerticalInput() < 0) movement.AlterMovementSpeed("wb");
+        else movement.AlterMovementSpeed("w"); 
+
+
     }
 
     void ExitState(MovementSateManager movement, MovementBaseState state)
