1dc585a201febfb0c7a30e350ca1c2d75dc5fef0
Akshit Sharma <148575789+AkshitGameDev@users.noreply.github.com>
2025-12-23 19:53:07 -0500

    shooting implimentation 1/2

--- Explorer3D/Assets/scripts/KeyBindings.cs
+++ Explorer3D/Assets/scripts/KeyBindings.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class KeyBindings : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    KeyCode Forward = KeyCode.W;
+    KeyCode Backwards = KeyCode.S;
+    KeyCode Left = KeyCode.A;
+    KeyCode Right = KeyCode.D;
+    KeyCode Sprint = KeyCode.LeftShift;
+    KeyCode crouch = KeyCode.LeftControl;
+    KeyCode Shoot = KeyCode.Mouse0;
+
+    public static KeyBindings instance;
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public KeyCode GetForwardKey() { return Forward; }
+    public KeyCode GetBackwardsKey() { return Backwards; }
+    public KeyCode GetLeftKey() { return Left; }
+
+    public KeyCode GetRightKey() { return Right; }
+    public KeyCode GetSprintKey() { return Sprint; }
+    public KeyCode GetCrouchKey() { return crouch; }
+    public KeyCode GetShootKey() { return Shoot; }
+
+
+
+
+
 }
---
+++ Explorer3D/Assets/scripts/WeponManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class WeponManager : MonoBehaviour
+{
+    [SerializeField]
+    float fireRate;
+
+    float fireRateTimer;
+
+    [SerializeField]
+    bool semiAuto;
+
+    KeyCode shoot = KeyBindings.instance.GetShootKey();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        fireRateTimer = fireRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool ShouldFire()
+    {
+        if(fireRateTimer < fireRate)
+        {
+            fireRateTimer += Time.deltaTime;
+            return false;
+        }
+
+        if(semiAuto && Input.GetKeyDown(shoot)) return true;
+        if(!semiAuto && Input.GetKeyDown(shoot)) return true;
+
+        return false;
+
+
+    }
+
+    void Fire()
+    {
+
+    }
+}
